18e90547d661012a17a5350653293805fb6b3aea
Geno McNew <genemcnew3@gmail.com>
2020-08-12 01:20:39 -0500

    Shark Attack Moves from Tank to Graveyard

--- src/App/App.js
+++ src/App/App.js
@@ -16,25 +16,17 @@ class App extends React.Component {
     this.setState({ livingStudents, deadStudents });
   }
 
-  // eslint-disable-next-line consistent-return
-  getLiveId = () => {
-    console.warn('randomizer called');
-    const livingStudents = studentsData.livingStudents();
-    const randomId = Math.floor(Math.random() * 22);
-    const randomStudent = livingStudents.filter((student) => student.id === randomId);
-    if (randomStudent[0] !== undefined) {
-      return randomStudent[0].id;
-    } this.getLiveId();
-  }
-
   sharkAttack = (e) => {
     e.preventDefault();
-    let studentId = this.getLiveId();
-    console.warn(studentId);
-    studentsData.followTheLight(studentId);
-    const livingStudents = studentsData.livingStudents();
-    const deadStudents = studentsData.dearlyBeloved();
-    this.setState({ livingStudents, deadStudents });
+    const sharkBait = studentsData.livingStudents();
+    if (sharkBait.length > 0) {
+      const randomIndex = Math.floor(Math.random() * sharkBait.length);
+      const randomStudent = sharkBait[randomIndex];
+      studentsData.followTheLight(randomStudent.id);
+      const livingStudents = studentsData.livingStudents();
+      const deadStudents = studentsData.dearlyBeloved();
+      this.setState({ livingStudents, deadStudents });
+    }
   }
 
   render() {
